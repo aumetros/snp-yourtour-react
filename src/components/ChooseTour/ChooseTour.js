d235e4763ada28942021a4290fc258dda68d1668
aumetros <aum3tros@yandex.ru>
2023-09-05 23:09:22 +0300

    Add ChooseTour: Create component TourCard and mapping

--- src/components/ChooseTour/ChooseTour.js
+++ src/components/ChooseTour/ChooseTour.js
@@ -1,5 +1,7 @@
 import "./ChooseTour.css";
 import TourTypes from "../TourTypes/TourTypes";
+import TourCard from "../TourCard/TourCard";
+import { arrCards } from "../../utils/useData";
 
 function ChooseTour() {
   return (
@@ -7,114 +9,9 @@ function ChooseTour() {
       <h2 className="choose-tour__title text-section-title">Выбери свой тур</h2>
       <TourTypes />
       <ul className="choose-tour__cards">
-        <li className="tours-card">
-          <img
-            className="tours-card__foto"
-            src="./images/card-tour-photo-1.jpg"
-            alt="tour foto"
-          />
-          <div className="tours-card__text-container">
-            <h3 className="tours-card__title text-card-title">
-              Путешествие в&nbsp;горы
-            </h3>
-            <p className="tours-card__subtitle">от 80 000 руб</p>
-          </div>
-          <button className="tours-card__forward-button text-bold">
-            <span>Подробнее</span>
-            <span className="tours-card__button-arrow"></span>
-          </button>
-          <div className="tours-card__overlay"></div>
-        </li>
-        <li className="tours-card">
-          <img
-            className="tours-card__foto"
-            src="./images/card-tour-photo-2.jpg"
-            alt="tour foto"
-          />
-          <div className="tours-card__text-container">
-            <h3 className="tours-card__title text-card-title">
-              Путешествие в&nbsp;горы
-            </h3>
-            <p className="tours-card__subtitle">от 80 000 руб</p>
-          </div>
-          <button className="tours-card__forward-button text-bold">
-            <span>Подробнее</span>
-            <span className="tours-card__button-arrow"></span>
-          </button>
-          <div className="tours-card__overlay"></div>
-        </li>
-        <li className="tours-card">
-          <img
-            className="tours-card__foto"
-            src="./images/card-tour-photo-3.jpg"
-            alt="tour foto"
-          />
-          <div className="tours-card__text-container">
-            <h3 className="tours-card__title text-card-title">
-              Путешествие в&nbsp;горы
-            </h3>
-            <p className="tours-card__subtitle">от 80 000 руб</p>
-          </div>
-          <button className="tours-card__forward-button text-bold">
-            <span>Подробнее</span>
-            <span className="tours-card__button-arrow"></span>
-          </button>
-          <div className="tours-card__overlay"></div>
-        </li>
-        <li className="tours-card">
-          <img
-            className="tours-card__foto"
-            src="./images/card-tour-photo-4.jpg"
-            alt="tour foto"
-          />
-          <div className="tours-card__text-container">
-            <h3 className="tours-card__title text-card-title">
-              Путешествие в&nbsp;горы
-            </h3>
-            <p className="tours-card__subtitle">от 80 000 руб</p>
-          </div>
-          <button className="tours-card__forward-button text-bold">
-            <span>Подробнее</span>
-            <span className="tours-card__button-arrow"></span>
-          </button>
-          <div className="tours-card__overlay"></div>
-        </li>
-        <li className="tours-card">
-          <img
-            className="tours-card__foto"
-            src="./images/card-tour-photo-5.jpg"
-            alt="tour foto"
-          />
-          <div className="tours-card__text-container">
-            <h3 className="tours-card__title text-card-title">
-              Путешествие в&nbsp;горы
-            </h3>
-            <p className="tours-card__subtitle">от 80 000 руб</p>
-          </div>
-          <button className="tours-card__forward-button text-bold">
-            <span>Подробнее</span>
-            <span className="tours-card__button-arrow"></span>
-          </button>
-          <div className="tours-card__overlay"></div>
-        </li>
-        <li className="tours-card">
-          <img
-            className="tours-card__foto"
-            src="./images/card-tour-photo-6.jpg"
-            alt="tour foto"
-          />
-          <div className="tours-card__text-container">
-            <h3 className="tours-card__title text-card-title">
-              Путешествие в&nbsp;горы
-            </h3>
-            <p className="tours-card__subtitle">от 80 000 руб</p>
-          </div>
-          <button className="tours-card__forward-button text-bold">
-            <span>Подробнее</span>
-            <span className="tours-card__button-arrow"></span>
-          </button>
-          <div className="tours-card__overlay"></div>
-        </li>
+        {arrCards.map((card, index) => {
+          return <TourCard key={index} card={card} />;
+        })}
       </ul>
     </section>
   );
